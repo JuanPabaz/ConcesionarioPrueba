0e51bd7a0bd55d329124e3fdf5535fd0d049b7f9
JuanPabaz <jpgthf@gmail.com>
2024-12-04 10:28:41 -0500

    feat: se implementa el metodo buscarVehiculoPorId

--- src/main/java/com/pruebatecnica/concesionario/service/VehiculoServiceImpl.java
+++ src/main/java/com/pruebatecnica/concesionario/service/VehiculoServiceImpl.java
@@ -1,11 +1,13 @@
 package com.pruebatecnica.concesionario.service;
 
 import com.pruebatecnica.concesionario.dto.VehiculoDTO;
+import com.pruebatecnica.concesionario.entities.Vehiculo;
 import com.pruebatecnica.concesionario.maps.IMapVehiculo;
 import com.pruebatecnica.concesionario.repositories.VehiculoRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehiculoServiceImpl implements IVehiculoService {
@@ -23,5 +25,11 @@ public class VehiculoServiceImpl implements IVehiculoService {
     @Override
     public List<VehiculoDTO> listarVehiculos() {
         return mapVehiculo.mapVehiculoList(vehiculoRepository.findAll());
+    }
+
+    @Override
+    public Vehiculo buscarVehiculoPorId(Long id) {
+        Optional<Vehiculo> vehiculoOptional = vehiculoRepository.findById(id);
+        return vehiculoOptional.orElse(null);
     }
 }
