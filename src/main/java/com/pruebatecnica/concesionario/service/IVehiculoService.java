9da3ce30fdf39f40c4500ca5665500bac3914af4
JuanPabaz <jpgthf@gmail.com>
2024-12-04 10:05:13 -0500

    feat: crear la interfaz de servicio de vehiculo

---
+++ src/main/java/com/pruebatecnica/concesionario/service/IVehiculoService.java
@@ -0,0 +1,10 @@
+package com.pruebatecnica.concesionario.service;
+
+import com.pruebatecnica.concesionario.dto.VehiculoDTO;
+
+import java.util.List;
+
+public interface IVehiculoService {
+
+    List<VehiculoDTO> listarVehiculos();
+}
