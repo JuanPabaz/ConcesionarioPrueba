41ae91881ee3d313892ff630e53c26500456edd4
JuanPabaz <jpgthf@gmail.com>
2024-12-04 10:05:17 -0500

    feat: crear la interfaz de servicio de orden

---
+++ src/main/java/com/pruebatecnica/concesionario/service/IOrdenService.java
@@ -0,0 +1,10 @@
+package com.pruebatecnica.concesionario.service;
+
+import com.pruebatecnica.concesionario.dto.OrdenDTO;
+
+import java.util.List;
+
+public interface IOrdenService {
+
+    List<OrdenDTO> listarOrdenes();
+}
