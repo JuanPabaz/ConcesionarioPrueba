18c086fa2633d087a0e21b21a178de7195dbb31c
JuanPabaz <jpgthf@gmail.com>
2024-12-04 10:04:03 -0500

    feat: configurar la entidad orden

---
+++ src/main/java/com/pruebatecnica/concesionario/entities/Orden.java
@@ -0,0 +1,35 @@
+package com.pruebatecnica.concesionario.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "orden")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Orden {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "vehiculo_id", nullable = false)
+    private Vehiculo vehiculo;
+
+    @Column(nullable = false)
+    private LocalDate fechaOrden;
+
+    @Column(nullable = false)
+    private String tipo;
+
+    @Column(nullable = false)
+    private Boolean activa;
+}
