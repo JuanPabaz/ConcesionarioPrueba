a805eda0bf173a54e0692c0e6fdd49fbaa00a7ec
JuanPabaz <jpgthf@gmail.com>
2024-12-04 10:27:16 -0500

    feat: se crea la firma del metodo crearOrden

--- src/main/java/com/pruebatecnica/concesionario/service/IOrdenService.java
+++ src/main/java/com/pruebatecnica/concesionario/service/IOrdenService.java
@@ -1,10 +1,13 @@
 package com.pruebatecnica.concesionario.service;
 
 import com.pruebatecnica.concesionario.dto.OrdenDTO;
+import com.pruebatecnica.concesionario.entities.Orden;
 
 import java.util.List;
 
 public interface IOrdenService {
 
     List<OrdenDTO> listarOrdenes();
+
+    OrdenDTO crearOrden(Orden orden) throws Exception;
 }
