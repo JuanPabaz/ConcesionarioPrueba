ca545e9d04bb5ea782421e120cc3f918aa05cf16
JuanPabaz <jpgthf@gmail.com>
2024-12-04 10:28:11 -0500

    feat: se utilizan los JsonBackReference y JsonManagedReference en las relaciones necsesarias

--- src/main/java/com/pruebatecnica/concesionario/entities/Vehiculo.java
+++ src/main/java/com/pruebatecnica/concesionario/entities/Vehiculo.java
@@ -1,5 +1,6 @@
 package com.pruebatecnica.concesionario.entities;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -30,6 +31,7 @@ public class Vehiculo {
     private String modelo;
 
     @OneToMany(targetEntity = Orden.class,mappedBy = "vehiculo", orphanRemoval = true, cascade = CascadeType.ALL)
+    @JsonManagedReference
     private List<Orden> ordenes;
 
 }
