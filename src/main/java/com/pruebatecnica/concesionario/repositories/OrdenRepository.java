0278e16a65d230cc13792853316e0dab6f61062c
JuanPabaz <jpgthf@gmail.com>
2024-12-04 10:29:00 -0500

    feat: se añade la query de encontrar si el vehiculo tiene una orden activa

--- src/main/java/com/pruebatecnica/concesionario/repositories/OrdenRepository.java
+++ src/main/java/com/pruebatecnica/concesionario/repositories/OrdenRepository.java
@@ -2,8 +2,13 @@ package com.pruebatecnica.concesionario.repositories;
 
 import com.pruebatecnica.concesionario.entities.Orden;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrdenRepository extends JpaRepository<Orden, Long> {
 
-
+    @Query("SELECT o FROM Orden AS o WHERE o.vehiculo.id = :idVehiculo AND o.activa = true")
+    Optional<List<Orden>> encontrarPorIdVehiculoYOrdenActiva(Long idVehiculo);
 }
