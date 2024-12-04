0d71e3c8af62db56d07e2a5b0e004befd3f49de8
JuanPabaz <jpgthf@gmail.com>
2024-12-04 10:27:32 -0500

    feat: se implementa el metodo crearOrden

--- src/main/java/com/pruebatecnica/concesionario/service/OrdenServiceImpl.java
+++ src/main/java/com/pruebatecnica/concesionario/service/OrdenServiceImpl.java
@@ -1,11 +1,16 @@
 package com.pruebatecnica.concesionario.service;
 
 import com.pruebatecnica.concesionario.dto.OrdenDTO;
+import com.pruebatecnica.concesionario.dto.VehiculoDTO;
+import com.pruebatecnica.concesionario.entities.Orden;
+import com.pruebatecnica.concesionario.entities.Vehiculo;
 import com.pruebatecnica.concesionario.maps.IMapOrden;
 import com.pruebatecnica.concesionario.repositories.OrdenRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrdenServiceImpl implements IOrdenService{
@@ -14,13 +19,37 @@ public class OrdenServiceImpl implements IOrdenService{
 
     private final IMapOrden mapOrden;
 
-    public OrdenServiceImpl(OrdenRepository ordenRepository, IMapOrden mapOrden) {
+    private final IVehiculoService vehiculoService;
+
+    public OrdenServiceImpl(OrdenRepository ordenRepository, IMapOrden mapOrden, IVehiculoService vehiculoService) {
         this.ordenRepository = ordenRepository;
         this.mapOrden = mapOrden;
+        this.vehiculoService = vehiculoService;
     }
 
     @Override
     public List<OrdenDTO> listarOrdenes() {
         return mapOrden.mapOrdenList(ordenRepository.findAll());
+    }
+
+    @Override
+    public OrdenDTO crearOrden(Orden orden) throws Exception {
+        Vehiculo vehiculo = vehiculoService.buscarVehiculoPorId(orden.getVehiculo().getId());
+        if (vehiculo == null) {
+            throw new Exception("El vehiculo no existe");
+        }
+
+        Optional<List<Orden>> optionalOrdenActivaList = ordenRepository.encontrarPorIdVehiculoYOrdenActiva(vehiculo.getId());
+        if (optionalOrdenActivaList.isPresent()) {
+            throw new Exception("El vehiculo ya tiene una orden activa");
+        }
+
+        LocalDate fechaActual = LocalDate.now();
+        if (orden.getFechaOrden().isAfter(fechaActual)){
+            throw new Exception("No se puede crear una orden con fecha mayor a la actual:" + fechaActual);
+        }
+
+
+        return null;
     }
 }
