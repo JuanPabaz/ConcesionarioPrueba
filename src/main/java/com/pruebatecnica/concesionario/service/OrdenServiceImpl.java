0b5bde8ee2d070dd4ccc48e4fe2f93f9a62f3ef7
JuanPabaz <jpgthf@gmail.com>
2024-12-04 10:46:19 -0500

    feat: se utiliza le excepcion BadCreateRequest

--- src/main/java/com/pruebatecnica/concesionario/service/OrdenServiceImpl.java
+++ src/main/java/com/pruebatecnica/concesionario/service/OrdenServiceImpl.java
@@ -4,6 +4,7 @@ import com.pruebatecnica.concesionario.dto.OrdenDTO;
 import com.pruebatecnica.concesionario.dto.VehiculoDTO;
 import com.pruebatecnica.concesionario.entities.Orden;
 import com.pruebatecnica.concesionario.entities.Vehiculo;
+import com.pruebatecnica.concesionario.exceptions.BadCreateRequest;
 import com.pruebatecnica.concesionario.maps.IMapOrden;
 import com.pruebatecnica.concesionario.repositories.OrdenRepository;
 import org.springframework.stereotype.Service;
@@ -33,23 +34,22 @@ public class OrdenServiceImpl implements IOrdenService{
     }
 
     @Override
-    public OrdenDTO crearOrden(Orden orden) throws Exception {
+    public OrdenDTO crearOrden(Orden orden) throws BadCreateRequest {
         Vehiculo vehiculo = vehiculoService.buscarVehiculoPorId(orden.getVehiculo().getId());
         if (vehiculo == null) {
-            throw new Exception("El vehiculo no existe");
+            throw new BadCreateRequest("El vehiculo no existe");
         }
 
         Optional<List<Orden>> optionalOrdenActivaList = ordenRepository.encontrarPorIdVehiculoYOrdenActiva(vehiculo.getId());
         if (optionalOrdenActivaList.isPresent()) {
-            throw new Exception("El vehiculo ya tiene una orden activa");
+            throw new BadCreateRequest("El vehiculo ya tiene una orden activa");
         }
 
         LocalDate fechaActual = LocalDate.now();
         if (orden.getFechaOrden().isAfter(fechaActual)){
-            throw new Exception("No se puede crear una orden con fecha mayor a la actual:" + fechaActual);
+            throw new BadCreateRequest("No se puede crear una orden con fecha mayor a la actual:" + fechaActual);
         }
 
-
-        return null;
+        return mapOrden.mapOrden(ordenRepository.save(orden));
     }
 }
