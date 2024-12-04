34cda072d61da62af2670db4cd1290aa0ee7debd
JuanPabaz <jpgthf@gmail.com>
2024-12-04 11:47:20 -0500

    feat: se añadió la query para buscar el vehiculo segun la placa

--- src/main/java/com/pruebatecnica/concesionario/repositories/VehiculoRepository.java
+++ src/main/java/com/pruebatecnica/concesionario/repositories/VehiculoRepository.java
@@ -2,6 +2,12 @@ package com.pruebatecnica.concesionario.repositories;
 
 import com.pruebatecnica.concesionario.entities.Vehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
+
+    @Query("SELECT v FROM Vehiculo AS v WHERE v.placa = :placa")
+    Optional<Vehiculo> findByPlaca(String placa);
 }
