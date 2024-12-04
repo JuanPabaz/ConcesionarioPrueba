f1e2f43658853d335006ca3a7e2511d7ccbc3a53
JuanPabaz <jpgthf@gmail.com>
2024-12-04 10:28:29 -0500

    feat: se crea la firma del metodo buscarVehiculoPorId

--- src/main/java/com/pruebatecnica/concesionario/service/IVehiculoService.java
+++ src/main/java/com/pruebatecnica/concesionario/service/IVehiculoService.java
@@ -1,10 +1,13 @@
 package com.pruebatecnica.concesionario.service;
 
 import com.pruebatecnica.concesionario.dto.VehiculoDTO;
+import com.pruebatecnica.concesionario.entities.Vehiculo;
 
 import java.util.List;
 
 public interface IVehiculoService {
 
     List<VehiculoDTO> listarVehiculos();
+
+    Vehiculo buscarVehiculoPorId(Long id);
 }
