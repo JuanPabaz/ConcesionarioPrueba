848f799ad6ade77741e8793a2a04397e4621a05b
JuanPabaz <jpgthf@gmail.com>
2024-12-04 11:48:37 -0500

    feat: se añadió la excepcion personalizada ObjectNotFoundException

---
+++ src/main/java/com/pruebatecnica/concesionario/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.pruebatecnica.concesionario.exceptions;
+
+public class ObjectNotFoundException extends Exception{
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
