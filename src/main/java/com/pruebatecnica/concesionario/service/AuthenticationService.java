3436963d8cff6fe0431b4a574469460ebbb10b08
JuanPabaz <jpgthf@gmail.com>
2024-12-04 14:12:51 -0500

    feat: se crea el servicio de autentication

---
+++ src/main/java/com/pruebatecnica/concesionario/service/AuthenticationService.java
@@ -0,0 +1,68 @@
+package com.pruebatecnica.concesionario.service;
+
+import com.pruebatecnica.concesionario.dto.UsuarioResponseDTO;
+import com.pruebatecnica.concesionario.entities.Usuario;
+import com.pruebatecnica.concesionario.enums.Role;
+import com.pruebatecnica.concesionario.exceptions.BadUserCredentialsException;
+import com.pruebatecnica.concesionario.exceptions.ObjectNotFoundException;
+import com.pruebatecnica.concesionario.maps.IMapUsuario;
+import com.pruebatecnica.concesionario.repositories.UsuarioRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class AuthenticationService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtServiceImpl jwtService;
+
+    private final RefreshTokenService refreshTokenService;
+
+    private final IMapUsuario mapUsuario;
+
+    public AuthenticationService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtServiceImpl jwtService, RefreshTokenService refreshTokenService, IMapUsuario mapUsuario) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        this.refreshTokenService = refreshTokenService;
+        this.mapUsuario = mapUsuario;
+    }
+
+
+    public UsuarioResponseDTO saveUser(Usuario usuario) throws BadUserCredentialsException {
+        if (usuarioRepository.findByUsername(usuario.getUsername()).isPresent()){
+            throw new BadUserCredentialsException("Ya existe un usuario con este correo: "+ usuario.getUsername() + ".");
+        }
+
+        String passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d/*\\-_.º!?¿'¡#!$%&]{6,}$";
+        if (!usuario.getPassword().matches(passwordRegex)){
+            throw new BadUserCredentialsException("La contraseña debe tener al menos 6 caracteres y contener al menos una letra y un número.");
+        }
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        if (!usuario.getUsername().matches(emailRegex)){
+            throw new BadUserCredentialsException("El correo no es valido.");
+        }
+
+        usuario.setRole(Role.ADMIN);
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+
+        return mapUsuario.mapUsuario(usuarioRepository.save(usuario));
+    }
+
+    public String generateToken(String username) throws ObjectNotFoundException {
+        return jwtService.generateToken(username);
+    }
+
+    public Map<String, Object> validateToken(String token){
+        return jwtService.validateToken(token);
+    }
+}
