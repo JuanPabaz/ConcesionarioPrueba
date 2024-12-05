d2632e24bb1b0b42941014c9cbd5afd159509a54
JuanPabaz <jpgthf@gmail.com>
2024-12-04 21:54:33 -0500

    feat: se crea la clase de AuthenticationServiceTest y se hace el test de guardar un usuario y que lance excepcion si ya existe un usuario con ese correo

---
+++ src/test/java/com/pruebatecnica/concesionario/service/AuthenticationServiceTest.java
@@ -0,0 +1,59 @@
+package com.pruebatecnica.concesionario.service;
+
+import com.pruebatecnica.concesionario.dto.UsuarioResponseDTO;
+import com.pruebatecnica.concesionario.entities.Usuario;
+import com.pruebatecnica.concesionario.enums.Role;
+import com.pruebatecnica.concesionario.exceptions.BadUserCredentialsException;
+import com.pruebatecnica.concesionario.exceptions.ObjectNotFoundException;
+import com.pruebatecnica.concesionario.maps.IMapUsuario;
+import com.pruebatecnica.concesionario.repositories.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class AuthenticationServiceTest {
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private JwtServiceImpl jwtService;
+
+    @Mock
+    private IMapUsuario mapUsuario;
+
+    @InjectMocks
+    private AuthenticationService authenticationService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void saveUser_debeLanzarExcepcionSiUsuarioYaExiste() {
+        // Arrange
+        Usuario usuario = new Usuario();
+        usuario.setUsername("test@example.com");
+
+        when(usuarioRepository.findByUsername("test@example.com")).thenReturn(Optional.of(usuario));
+
+        // Act & Assert
+        BadUserCredentialsException exception = assertThrows(BadUserCredentialsException.class, () -> authenticationService.saveUser(usuario));
+        assertEquals("Ya existe un usuario con este correo: test@example.com.", exception.getMessage());
+    }
+
+}
