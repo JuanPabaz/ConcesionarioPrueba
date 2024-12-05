d48738135e219e0fbb6d6651858b8af824340f9a
JuanPabaz <jpgthf@gmail.com>
2024-12-04 22:05:09 -0500

    feat: se crea la clase RefreshTokenServiceTest para el testing de RefreshTokenService y se crea el test para crear y guardar el refresh token correctamente

---
+++ src/test/java/com/pruebatecnica/concesionario/service/RefreshTokenServiceTest.java
@@ -0,0 +1,64 @@
+package com.pruebatecnica.concesionario.service;
+
+import com.pruebatecnica.concesionario.entities.RefreshToken;
+import com.pruebatecnica.concesionario.entities.Usuario;
+import com.pruebatecnica.concesionario.exceptions.ExpiredRefreshTokenException;
+import com.pruebatecnica.concesionario.repositories.RefreshTokenRepository;
+import com.pruebatecnica.concesionario.repositories.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class RefreshTokenServiceTest {
+
+    @Mock
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @InjectMocks
+    private RefreshTokenService refreshTokenService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void createRefreshToken_debeCrearYGuardarToken() {
+        // Arrange
+        String username = "testuser";
+        Usuario usuario = new Usuario();
+        usuario.setUsername(username);
+        Instant beforeCreation = Instant.now();
+
+        when(usuarioRepository.findByUsername(username)).thenReturn(Optional.of(usuario));
+        when(refreshTokenRepository.save(any(RefreshToken.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // Act
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(username);
+
+        // Assert
+        assertNotNull(refreshToken);
+        assertEquals(usuario, refreshToken.getUsuario());
+        assertNotNull(refreshToken.getToken());
+        assertTrue(refreshToken.getExpiryDate().isAfter(
+                beforeCreation),
+                "El tiempo de expiración debe ser posterior al tiempo antes de la creación.");
+        verify(usuarioRepository).findByUsername(username);
+        verify(refreshTokenRepository).save(any(RefreshToken.class));
+    }
+
+}
